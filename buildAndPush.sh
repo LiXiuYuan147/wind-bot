c825b4c8458c3eec9bc4e38ed687e0593461c28a
xiuyuan <2901427129@qq.com>
2024-05-17 12:00:53 +0800

    feat(基础): 打包和发布相关调整

---
+++ buildAndPush.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+##登录测试 docker 仓库
+CI_REGISTRY_TEST=registry.cn-hangzhou.aliyuncs.com
+CI_PROJECT_PATH=xiuyuan-fenger
+BUILD_VERSION=latest
+
+docker login -u 15934729199 -p FengYe@233 $CI_REGISTRY_TEST
+
+###################################### blog-manager ##############################################
+ service_name="wind"
+  {
+  echo "docker build $CI_REGISTRY_TEST/$CI_PROJECT_PATH/${service_name}:$BUILD_VERSION"
+  docker build . -t "$CI_REGISTRY_TEST/$CI_PROJECT_PATH/${service_name}:$BUILD_VERSION" -f service/Dockerfile &&
+  if [ $? -ne 0 ]; then
+     export errormsg="$errormsg \n ${service_name}像构建失败"
+  fi
+ docker push "$CI_REGISTRY_TEST/$CI_PROJECT_PATH/${service_name}:$BUILD_VERSION"
+   if [ $? -ne 0 ]; then
+     export errormsg="$errormsg \n ${service_name}像推送失败"
+   fi
+ }
+wait
+
+if [ -n "$errormsg" ]; then
+  echo "构建失败： \n $errormsg"
+  exit 1
+fi
+
+echo "镜像构建成功"
+exit 0
+
--- wind-bot-service/src/test/java/woaini/fenger/bot/BotApplicationTest.java
+++ wind-bot-service/src/test/java/woaini/fenger/bot/BotApplicationTest.java
@@ -6,13 +6,19 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 import xx.wind.app.BotApplication;
 
+import java.io.UnsupportedEncodingException;
+
 @Slf4j
 @Test
 @SpringBootTest(classes = BotApplication.class)
 public class BotApplicationTest extends AbstractTestNGSpringContextTests {
 
-    @Test
-    public void test(){
-    System.out.println(1);
-    }
+  @Test
+  public void test() {}
+
+  public static void main(String[] args) throws UnsupportedEncodingException {
+    String s = "我不知道啊";
+    byte[] bytes = s.getBytes("ISO8859-1");
+    System.out.println(new String(bytes));
+  }
 }
